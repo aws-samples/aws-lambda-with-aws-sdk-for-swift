9a09c491e3586ed3e425216c01139aac3456bed2
David Moser <davmoser@amazon.com>
2021-09-22 10:23:19 -0700

    bump to Swift 5.5, CDK 1.124, Lambda runtime .0.5.1

--- lambda/functions/swift-lambda-function/Package.swift
+++ lambda/functions/swift-lambda-function/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "swift-lambda-function",
     platforms: [.macOS(.v10_15)],
     dependencies: [
-        .package(name: "swift-aws-lambda-runtime", url: "https://github.com/swift-server/swift-aws-lambda-runtime", from: "0.5.0"),
+        .package(name: "swift-aws-lambda-runtime", url: "https://github.com/swift-server/swift-aws-lambda-runtime", from: "0.5.1"),
         .package(name: "AWSSwiftSDK", url: "https://github.com/awslabs/aws-sdk-swift", from: "0.0.8")
     ],
     targets: [
